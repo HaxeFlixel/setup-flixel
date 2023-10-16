e8340459b4cceaf79c18af1651c0b4744df52039
George FunBook <gkurelic@gmail.com>
2023-10-16 17:39:10 -0500

    use geokureli spinehaxe

--- src/Main.hx
+++ src/Main.hx
@@ -104,7 +104,7 @@ private function installHaxelibs(flixelVersions):ExitCode {
 		Haxelib.install.bind("haxeui-flixel"),
 		Haxelib.git.bind("HaxeFoundation", "hscript"),
 		Haxelib.git.bind("larsiusprime", "firetongue"),
-		Haxelib.git.bind("bendmorris", "spinehaxe"),
+		Haxelib.git.bind("Geokureli", "spinehaxe", "spinehaxe", "haxe4.3.1"),
 		Haxelib.git.bind("larsiusprime", "steamwrap"),
  
 		Haxelib.install.bind("openfl"),
